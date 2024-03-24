a2533009d4cf9f44550f1f9aa01b1c4f46c59004
Romain Delbos <nostromo_site@yahoo.fr>
2024-03-24 16:25:15 +0100

    Ajout champ "pseudonyme" dans formulaire d'inscription.

--- src/pages/Inscription.jsx
+++ src/pages/Inscription.jsx
@@ -8,35 +8,29 @@ import { toast } from "react-toastify";
 export default function Home() {
  const [errorMessagePasswords, setErrorMessagePasswords] = useState();
   // States
-  const { register, handleSubmit,formState: { errors },} = useForm();
+  const { register, handleSubmit, formState: { errors },} = useForm();
   // register sert enregistrer le champ du formulaire
   // handleSubmit sert à envoyer le contenu du formulaire
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+
+  // Function
+  const sendDataToFirebase = async (data) => {
+
+    if (data.password !== data.passwordConfirm) {
+      console.log("Les deux champs de mot de passe ne sont pas identiques.");
+      setErrorMessagePasswords("Les deux champs de mot de passe doivent être identiques.")
+      return;
+    }
+
+    if (loading) return;
+    setLoading(true);
 /*
   Dans create..., on utilise l'argument auth, puis l'email, puis le mot de passe. Firebase va créer notre utilisateur.
   Ensuite, userCredential va récupérer le fait que l'utilisateur est bien connecté.
   Dans user, on stocke l'utilisateur.
 */
-  // Function
-
-
-
-
-  const sendDataToFirebase = async (data) => {
-
-    if (data.password !== data.passwordConfirm) {
-
-      console.log("Les deux champs de mot de passe ne sont pas identiques.");
-      setErrorMessagePasswords("Les deux champs de mot de passe doivent être identiques.")
-      return;
-
-    }
-
-    if (loading) return;
-    setLoading(true);
-
-    await createUserWithEmailAndPassword(auth, data.email, data.password, data.name).then(
+    await createUserWithEmailAndPassword(auth, data.email, data.password).then(
       (userCredential) => {
         const user = userCredential.user;
         setLoading(false);
@@ -49,8 +43,34 @@ export default function Home() {
         }
         else{toast.error(message);}        
         setLoading(false);
-      })
-    ;
+      });
+
+/*************************Création d'un pseudonyme dans Realtime Database ********************************************************/
+/*
+Ici, les données créées seront envoyées dans RealtimeDatabase.
+On y envoie l'adresse  mail qui servira d'identifiant permanent
+Et le pseudo choici par l'utilisateur. Ce pseudo pourra être changer n'importe quand. C'est pour cette raison qu'il ne servira pas d'identifiant.
+*/
+      const userData = {pseudonymUser : data.pseudonymUser, idUser : data.email}
+
+      const newUser = await fetch(// Une nouvelle section  dans realtime database : userList.
+        "https://projet-passerelle-3-believemy-default-rtdb.europe-west1.firebasedatabase.app/userList.json",
+                    {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(userData),
+                }
+            )
+    
+       // Error
+          if(!newUser.ok) {
+            return  "Une erreur est survenue. Impossible d'afficher la base de données.";        
+        }
+    
+        const {name: idRandom} = await newUser.json();
+        console.log("Le data.name généré aléatoirement dans Firebase par FormWriteTweet " + idRandom);
   };
   return (
     <>
@@ -59,7 +79,7 @@ export default function Home() {
           <div className="text-center text-lg mb-5">
             S&apos;inscrire
           </div>
-          {/* Form */}
+{/*------------- Form -----------------------------------------------------------------------------------*/}
           <form onSubmit={handleSubmit(sendDataToFirebase)}>
             <input
               style={errors.email && { background: "red" }}
@@ -79,11 +99,30 @@ export default function Home() {
                 {errors.email.message}
               </p>
             )}
-
-
+{/*------------- Pseudonym -----------------------------------------------------------------------------------*/}
             <input
-            className={errors.password ? "invalid-input" : ""}
+              className={errors.pseudonymUser ? "invalid-input" : ""}
               type="text"
+              placeholder="Votre pseudonyme"
+              name="pseudonymUser"
+              {...register("pseudonymUser", {
+                required: true,
+                maxLength: {
+                  value: 25,
+                  message:
+                    "Le mot de passe doit contenir au maximum 25 caractères",
+                },
+              })}
+            />
+            {errors.pseudonymUser && (
+              <p style={{ color: "red", fontSize: "12px", margin: "5px 0" }}>
+                {errors.pseudonymUser.message}
+              </p>
+            )}
+{/*------------- password ----------------------------------------------------------------------------------*/}
+            <input
+              className={errors.password ? "invalid-input" : ""}
+              type="password"
               placeholder="Mot de passe"
               name="password"
               {...register("password", {
@@ -99,12 +138,11 @@ export default function Home() {
               <p style={{ color: "red", fontSize: "12px", margin: "5px 0" }}>
                 {errors.password.message}
               </p>
-            )}
-            
-
+            )}            
+{/*------------- password confirm -----------------------------------------------------------------------------------*/}
             <input
-            className={errors.password ? "invalid-input" : ""}
-              type="text"
+              className={errors.password ? "invalid-input" : ""}
+              type="password"
               placeholder="Mot de passe"
               name="passwordConfirm"
               {...register("passwordConfirm", {
@@ -122,6 +160,7 @@ export default function Home() {
               </p>
             )}
 
+{/*------------- Bouton Inscription -----------------------------------------------------------------------------------*/}
 
             <button>S&apos;inscrire</button>
             <div style={{ color: "red" }}>{errorMessagePasswords}</div>
